f57db4ba802c5feb93768f1e3a4d51e5d8c702f9
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-17 01:45:39 +0100

    update config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
 require_once "config/config.php";
+require_once "libraries/user.php";
+require_once "models/user.php";
+require_once "models/tag.php";
 
 if(empty($_SESSION["log"])) {
     header("Location: " . $url . "public/pages/login.php");
 }
+$user = new user($conn);
+$userData = $user->getUserData($_SESSION["email"]);
--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $url = "http://localhost/benfillous-anass_tickets_manager/";
+
 class Database {
     private $host = "localhost";
     private $user = "root";
@@ -16,18 +17,27 @@ class Database {
         $sql = "CREATE TABLE IF NOT EXISTS user (user_id INT PRIMARY KEY AUTO_INCREMENT, username TEXT, email TEXT UNIQUE, password TEXT, picture LONGBLOB)";
         $this->conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS tag (tag_id INT PRIMARY KEY AUTO_INCREMENT, tag TEXT)";
+        $sql = "CREATE TABLE IF NOT EXISTS tag (tag_id INT PRIMARY KEY AUTO_INCREMENT, tag TEXT UNIQUE)";
         $this->conn->query($sql);
 
         $sql = "CREATE TABLE IF NOT EXISTS comment (comment_id INT PRIMARY KEY AUTO_INCREMENT, comment TEXT, user_id INT,
                 FOREIGN KEY (user_id) REFERENCES user(user_id))";
         $this->conn->query($sql);
 
-       $sql = "CREATE TABLE IF NOT EXISTS ticket (ticket_id INT PRIMARY KEY AUTO_INCREMENT, subject TEXT UNIQUE, description TEXT, status TEXT, priority text, date INT, deadline INT, is_deleted TINYINT(1) DEFAULT 0,
+       $sql = "CREATE TABLE IF NOT EXISTS ticket (ticket_id INT PRIMARY KEY AUTO_INCREMENT, subject TEXT UNIQUE, description TEXT, assignment TEXT, status TEXT, priority text, date INT, deadline INT, is_deleted TINYINT(1) DEFAULT 0,
             user_id INT, tag_id INT, comment_id INT,
             FOREIGN KEY (user_id) REFERENCES user(user_id),
             FOREIGN KEY (tag_id) REFERENCES tag(tag_id),
             FOREIGN KEY (comment_id) REFERENCES comment(comment_id))";
+        $this->conn->query($sql);
+
+        $sql = "INSERT INTO tag (tag) VALUES 
+    ('SoftwareIssue'), 
+    ('HardwareProblem'), 
+    ('NetworkTrouble'), 
+    ('SecurityConcern'), 
+    ('AccountAccess')
+    ON DUPLICATE KEY UPDATE tag = VALUES(tag)";
         $this->conn->query($sql);
     }
 
