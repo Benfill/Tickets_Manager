ebea8619267f21c110f5cc52470fb69c09f5ed90
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-17 22:14:38 +0100

    update ticket display

---
+++ app/controllers/ticket_display.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../../app/libraries/ticket.php";
+if(isset($_GET["ticket_id"])) {
+    $ticket_id = $_GET["ticket_id"];
+
+    $ticket = new ticket;
+    $ticketData = $ticket->displayTicket($conn, $ticket_id);
+    if(!$ticketData) {
+        header("Location: " . $url . "public/");
+    }
+} else {
+    header("Location: " . $url . "public/");
+}
