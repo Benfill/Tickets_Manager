babaf2d0c831dc883bff0157aaab29c25cb2a1cb
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-17 22:15:50 +0100

    update ticket display

--- app/libraries/ticket.php
+++ app/libraries/ticket.php
@@ -1,10 +1,11 @@
 <?php
 class ticket {
-    function createTicket ($subject, $deadline, $assignment, $tag, $priority, $status, $description, $date, $conn) {
+    function createTicket ($creator, $subject, $deadline, $assignment, $tag, $priority, $status, $description, $date, $conn) {
         require_once "../models/ticketModel.php";
         $ticketModel = new ticketModel();
 
         $ticketModel->__set("subject", $subject);
+        $ticketModel->__set("creator", $creator);
         $ticketModel->__set("assignment", $assignment);
         $ticketModel->__set("tag", $tag);
         $ticketModel->__set("priority", $priority);
@@ -12,7 +13,15 @@ class ticket {
         $ticketModel->__set("description", $description);
         $ticketModel->__set("deadline", $deadline);
         $ticketModel->__set("date", $date);
-        $ticketModel->insertTicket($conn);
+        return $ticketModel->insertTicket($conn);
+    }
+
+    function displayTicket($conn, $ticket_id) {
+        require_once "../../app/models/ticketModel.php";
+        $ticketModel = new ticketModel();
+
+        $ticketModel->__set("ticket_id", $ticket_id);
+        return $ticketModel->displayOneTicket($conn);
     }
 
 
