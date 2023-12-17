6604af51d3ffd8550a56ade16e753e2dcf203825
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-17 22:14:49 +0100

    update ticket form

--- app/controllers/ticket_form.php
+++ app/controllers/ticket_form.php
@@ -10,10 +10,13 @@ if (isset($_POST["save"])) {
     $status = filter_input(INPUT_POST, "status", FILTER_SANITIZE_SPECIAL_CHARS);
     $description = filter_input(INPUT_POST, "description", FILTER_SANITIZE_SPECIAL_CHARS);
     $date = date("U");
+    $creator = $_SESSION["user_id"];
 
     $deadline = filter_input(INPUT_POST, "deadline", FILTER_SANITIZE_NUMBER_INT);
     $deadlineObject = DateTime::createFromFormat('dmY', $deadline);
     $deadline = $deadlineObject->getTimestamp();
     $ticket = new ticket;
-    $ticket->createTicket($subject, $deadline, $assignment, $tag, $priority, $status, $description, $date, $conn);
+    $ticket_id = $ticket->createTicket($creator, $subject, $deadline, $assignment, $tag, $priority, $status, $description, $date, $conn);
+
+    header("Location: " . $url . "public/pages/ticket.php?ticket_id=". $ticket_id);
 }
