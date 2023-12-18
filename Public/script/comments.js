bb59fa83882ce03e4376ebcbc9c39e554e8fc71c
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-18 21:19:10 +0100

    add comment feature

--- Public/script/comments.js
+++ Public/script/comments.js
@@ -1,17 +1,54 @@
-//setInterval(ajaxCall, 6000);
-function ajaxCall() {
-    $.ajax(
-        '../../app/controllers/comment.php',
-        {
-            success: function(data) {
-                alert('AJAX call was successful!');
-                alert('Data from the server' + data);
-                console.log(data);
-            },
-            error: function(data) {
-               console.log(data);
+let commentSection = document.querySelector('#comment-section');
+let commentInput = document.querySelector('#comment');
+function ajaxCall(commentValue, userValue, ticketValue) {
+    $.ajax({
+        type: 'POST',
+        url: '../../app/controllers/comment.php',
+        data: {
+            comment: commentValue,
+            user_id: userValue,
+            ticket_id: ticketValue,
+        },
+        success: function(data) {
+            commentsData = JSON.parse(data);
+
+
+            console.log(commentsData);
+            commentSection.innerHTML = "";
+            for (let i = 0; i < commentsData.length; i++) {
+                commentText = commentsData[i].comment;
+                commentCreator = commentsData[i].user;
+                picture = commentsData[i].picture;
+            commentSection.innerHTML += `
+                    <div class="flex p-2">
+                    <img src="${picture}"
+                         class="h-10 w-10 rounded-full mr-2 object-cover border-2 border-yellow-500" />
+                    <div>
+                        <h1 class="font-bold text-gray-700 text-sm">${commentCreator}</h1>
+                        <!-- Comment Content Section -->
+                        <p class="text-gray-700">
+                            ${commentText}
+                        </p>
+                    </div>
+                    </div>`;
             }
+
+        },
+        error: function(xhr, status, error) {
+            console.error('AJAX call failed with status ' + status + ': ' + error);
         }
-    );
+    });
 }
+
+$('#commentForm').submit(function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    // Call the ajaxCall function
+    ajaxCall($('#comment').val(), $('#user_id').val(), $('#ticket_id').val());
+    commentInput.value = "";
+});
+
+// Call ajaxCall immediately
 ajaxCall();
+
+ setInterval(ajaxCall, 1000);
--- app/controllers/comment.php
+++ app/controllers/comment.php
@@ -2,7 +2,29 @@
 require_once "../config/config.php";
 require_once "../libraries/ticket.php";
 
-$comments = new ticket;
+$ticket = new ticket;
 $ticket_id = $_SESSION["ticket_id"];
-$comments = $comments->displayComments($conn, $ticket_id);
-echo $comments;
+if(isset($_POST["comment"])) {
+    // Form was submitted, process the data
+    $comment = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_SPECIAL_CHARS);
+    $user_id = $_POST["user_id"];
+    $ticket_id = $_POST["ticket_id"];
+    if ($comment !== "")
+        $ticket->insertComment($conn, $comment, $user_id, $ticket_id);
+    // Send a response back to the client (this is just an example)
+}
+
+
+$comments = $ticket->displayComments($conn, $ticket_id);
+
+foreach ($comments as $oneComment)
+$commentsData[] = array (
+    "comment" => $oneComment["comment"],
+    "user" => $oneComment["username"],
+    "picture" => $oneComment["picture"],
+);
+echo (json_encode($commentsData));
+
+
+
+
