1336c4cc238018b64f569e924da05564acbe97f0
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-18 21:37:39 +0100

    update picture handling

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -6,14 +6,21 @@ if (isset($_POST["submit"])) {
     $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $picture = file_get_contents($_FILES["picture"]["tmp_name"]);
     $password = password_hash($password, PASSWORD_BCRYPT);
 
-    try {
-        $user = new user($conn);
-        $userChecker = $user->register($username, $email, $password, $picture);
-        header("Location: " . $url . "public/pages/login.php?register=success");
-    } catch(Exception $e) {
-        header("Location: " . $url . "public/pages/login.php?error=" . $e->getMessage());
+    $targetDir = "../../public/img/";
+    $fileName = basename($_FILES["picture"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+
+    // Upload file to server
+    if (move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFilePath)) {
+
+        try {
+            $user = new user($conn);
+            $userChecker = $user->register($username, $email, $password, $targetFilePath);
+            header("Location: " . $url . "public/pages/login.php?register=success");
+        } catch(Exception $e) {
+            header("Location: " . $url . "public/pages/login.php?error=" . $e->getMessage());
+        }
     }
 }
