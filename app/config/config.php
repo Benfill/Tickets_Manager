44d06b62f2ca3d123fac00fe1586c08c1284b566
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-18 21:17:54 +0100

    add needed tables & update picture data type

--- app/config/config.php
+++ app/config/config.php
@@ -14,21 +14,26 @@ class Database {
         $sql = "CREATE DATABASE IF NOT EXISTS " . $this->dbName;
         $this->conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS user (user_id INT PRIMARY KEY AUTO_INCREMENT, username TEXT, email TEXT UNIQUE, password TEXT, picture LONGBLOB)";
+        $sql = "CREATE TABLE IF NOT EXISTS user (user_id INT PRIMARY KEY AUTO_INCREMENT, username TEXT, email TEXT UNIQUE, password TEXT, picture text)";
         $this->conn->query($sql);
 
         $sql = "CREATE TABLE IF NOT EXISTS tag (tag_id INT PRIMARY KEY AUTO_INCREMENT, tag TEXT UNIQUE)";
         $this->conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS comment (comment_id INT PRIMARY KEY AUTO_INCREMENT, comment TEXT, user_id INT,
+        $sql = "CREATE TABLE IF NOT EXISTS comment (comment_id INT PRIMARY KEY AUTO_INCREMENT, comment TEXT, user_id INT, ticket_id INT,
+                FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id),
                 FOREIGN KEY (user_id) REFERENCES user(user_id))";
         $this->conn->query($sql);
 
-       $sql = "CREATE TABLE IF NOT EXISTS ticket (ticket_id INT PRIMARY KEY AUTO_INCREMENT, subject TEXT, description TEXT, assignment INT, status TEXT, priority text, date INT, deadline INT, is_deleted TINYINT(1) DEFAULT 0,
+       $sql = "CREATE TABLE IF NOT EXISTS ticket (ticket_id INT PRIMARY KEY AUTO_INCREMENT, subject TEXT, description TEXT, status TEXT, priority text, date INT, deadline INT, is_deleted TINYINT(1) DEFAULT 0,
             user_id INT, tag_id INT,
-            FOREIGN KEY (assignment) REFERENCES user(user_id),
-            FOREIGN KEY (user_id) REFERENCES user(user_id),
+            FOREIGN KEY (user_id) REFERENCES user(user_id),  
             FOREIGN KEY (tag_id) REFERENCES tag(tag_id))";
+        $this->conn->query($sql);
+
+        $sql = "CREATE TABLE IF NOT EXISTS assignment (ticket_id INT, user_id INT,
+                FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id),
+                FOREIGN KEY (user_id) REFERENCES user(user_id))";
         $this->conn->query($sql);
 
         $sql = "INSERT INTO tag (tag) VALUES 
@@ -49,3 +54,5 @@ class Database {
 
 $database = new Database();
 $conn = $database->connection();
+
+
