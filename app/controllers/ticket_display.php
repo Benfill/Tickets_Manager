3138efd7b23f43f20e12677a74f352c2f1d4ca8c
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-18 01:26:17 +0100

    add ticket_id to the session

--- app/controllers/ticket_display.php
+++ app/controllers/ticket_display.php
@@ -2,6 +2,7 @@
 require_once "../../app/libraries/ticket.php";
 if(isset($_GET["ticket_id"])) {
     $ticket_id = $_GET["ticket_id"];
+    $_SESSION["ticket_id"] = $ticket_id;
 
     $ticket = new ticket;
     $ticketData = $ticket->displayTicket($conn, $ticket_id);
