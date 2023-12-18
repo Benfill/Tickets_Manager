9ddefe3d92717a74c2d01b394342ababa2a0bbee
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-19 00:35:14 +0100

    update path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,6 @@
 require_once "config/config.php";
 require_once "libraries/user.php";
 require_once "models/user.php";
-require_once "models/tag.php";
 
 $user = new user($conn);
 $userData = $user->getUserData($_SESSION["email"]);
