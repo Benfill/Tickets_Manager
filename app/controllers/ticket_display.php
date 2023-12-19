4614000f18c504f45944ff88d75c3887852c71a3
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-19 14:40:43 +0100

    dsiplay tags on ticket page

--- app/controllers/ticket_display.php
+++ app/controllers/ticket_display.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../../app/libraries/ticket.php";
 require_once "../../app/libraries/tag.php";
+require_once "../../app/models/tagModel.php";
 if(isset($_GET["ticket_id"])) {
     $ticket_id = $_GET["ticket_id"];
     $_SESSION["ticket_id"] = $ticket_id;
@@ -8,7 +9,8 @@ if(isset($_GET["ticket_id"])) {
     $ticket = new ticket;
     $ticketData = $ticket->displayTicket($conn, $ticket_id);
     $tag = new tag;
-    $tag->GetSpecificTags($conn, $ticket_id);
+    $tagModel = new tagModel;
+    $tags = $tag->GetSpecificTags($conn, $ticket_id, $tagModel);
     if(!$ticketData) {
         header("Location: " . $url . "public/");
     }
