063e38187ea1c0febd887e0312fbb57935239116
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-20 01:08:12 +0100

    update comments

--- app/controllers/comment.php
+++ app/controllers/comment.php
@@ -5,13 +5,11 @@ require_once "../libraries/ticket.php";
 $ticket = new ticket;
 $ticket_id = $_SESSION["ticket_id"];
 if(isset($_POST["comment"])) {
-    // Form was submitted, process the data
     $comment = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_SPECIAL_CHARS);
     $user_id = $_POST["user_id"];
     $ticket_id = $_POST["ticket_id"];
     if ($comment !== "")
         $ticket->insertComment($conn, $comment, $user_id, $ticket_id);
-    // Send a response back to the client (this is just an example)
 }
 
 
